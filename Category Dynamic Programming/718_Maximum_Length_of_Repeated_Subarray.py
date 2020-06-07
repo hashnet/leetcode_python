9193f273d885097402b5b8d7992edde880d35e24
Sheikh Mohammad Maidul Hasan <maidul.hasan@gmail.com>
2020-06-07 21:22:23 +1000

    Updated 718

--- Category Dynamic Programming/718_Maximum_Length_of_Repeated_Subarray.py
+++ Category Dynamic Programming/718_Maximum_Length_of_Repeated_Subarray.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 class Solution:
     # def findLength(self, A: List[int], B: List[int]) -> int:
@@ -23,29 +23,35 @@ class Solution:
 
     #     return maxim
 
-    def findLength(self, A: List[int], B: List[int]) -> int:
+    def findLength(self, A: List[int], B: List[int]) -> Tuple:
         memo = {}
-
-        def DP(i: int, j: int, matchCount: int) -> int:
-            if i<0 or j<0:
-                return matchCount
-                
-            if (i, j, matchCount) in memo:
-                return memo[(i, j, matchCount)]
-
-            maxim = 0
-            if A[i] == B[j]:
-                maxim = DP(i-1, j-1, matchCount+1)
-
-            maxim = max(maxim, max(DP(i-1, j, 0), DP(i, j-1, 0)))
-
-            memo[(i, j, matchCount)] = maxim
-            return maxim
-
         
-        return DP(len(A)-1, len(B)-1, 0)
+        def DP(i: int, j: int) -> int:
+            if i<0 or j<0:
+                return 0, 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+                
+            maxim = 0
+            matchedLength = 0
+            res = DP(i-1, j-1)
+            if A[i] == B[j]:
+                matchedLength = res[1] + 1
+                maxim = matchedLength
+
+            maxim = max(maxim, res[0])
+            maxim = max(maxim, DP(i-1, j)[0])
+            maxim = max(maxim, DP(i, j-1)[0])
+
+            memo[(i, j)] = maxim, matchedLength
+            return memo[(i, j)]
+        
+        return DP(len(A)-1, len(B)-1)[0]
 
 
 if __name__ == "__main__":
     s = Solution()
+    print(s.findLength([0,0,0,0,0], [0,0,0,0,0]))
+    print(s.findLength([5,14,53,80,48], [50,47,3,80,83]))
     print(s.findLength([1,2,3,2,1], [3,2,1,4,7]))
