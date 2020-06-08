1b1236302b8f4717d7ec9381a012a3a47c721a2b
Sheikh Mohammad Maidul Hasan <maidul.hasan@gmail.com>
2020-06-08 15:57:44 +1000

    Updated project directory structure

---
+++ all/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append("/Users/maidul/Python_Projects/leetcode_python/utility")
+
+from TreeUtility import *
+
+class Codec:
+
+    def __init__(self):
+        self.idx = 0
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if root is None:
+            return [None]
+
+        return [root.val] + self.serialize(root.left) + self.serialize(root.right)
+
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data[self.idx] == None:
+            self.idx += 1
+            return None
+        
+        node = TreeNode(data[self.idx])
+        self.idx += 1
+        node.left = self.deserialize(data)
+        node.right = self.deserialize(data)
+        return node
+
+        
+if __name__ == "__main__":
+    codec = Codec()
+    drawtree(codec.deserialize(codec.serialize(stringToTreeNode("[1,2,3,null,null,4,5]"))))
---
+++ utility/TreeUtility.py
@@ -0,0 +1,95 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return "TreeNode({})".format(self.val)
+
+
+def stringToTreeNode(input):
+    """Decodes a string to tree.
+    
+    :type input: str
+    :rtype: TreeNode
+    """
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left  = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
+
+
+def drawtree(root):
+    def height(root):
+        return 1 + max(height(root.left), height(root.right)) if root else -1
+
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y-20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x-dx, y-60, dx/2)
+            jumpto(x, y-20)
+            draw(node.right, x+dx, y-60, dx/2)
+
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0); turtle.delay(0)
+    h = height(root)
+    jumpto(0, 30*h)
+    draw(root, 0, 30*h, 40*h)
+    t.hideturtle()
+    turtle.mainloop()
+
+
+if __name__ == "__main__":
+    # drawtree(stringToTreeNode("[1,2,3,null,null,4,null,null,5]"))
+    drawtree(stringToTreeNode("[2,1,3,0,7,9,1,2,null,1,0,null,null,8,8,null,null,null,null,7]"))
